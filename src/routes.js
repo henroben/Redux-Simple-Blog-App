392d7182b1bab23fa1799b23dec9eeb8ffe588da
Ben Mercer <ben@henroben.co.uk>
2017-02-02 16:49:16 +0000

    S6:L92 - Show post component and dynamic routes added

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 export default (
 
@@ -11,6 +12,7 @@ export default (
 
         <IndexRoute component={PostsIndex} />
         <Route path="posts/new" component={PostsNew} />
+        <Route path="posts/:id" component={PostsShow} />
 
     </Route>
 
